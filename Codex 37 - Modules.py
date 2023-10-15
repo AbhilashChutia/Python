93bd15c94af21e6af23d4d0e1a7fc45a1bc81cf7
Ghost <75405213+AbhilashChutia@users.noreply.github.com>
2023-10-15 21:23:20 +0530

    Add files via upload

---
+++ Codex 37 - Modules.py
@@ -0,0 +1,8 @@
+# Module = File containing Python Code
+#  Used in Modular Programming
+# Seperate a program into parts
+
+import Messages as msg
+
+msg.hello()
+msg.Bye()
---
+++ Messages.py
@@ -0,0 +1,5 @@
+def hello():
+    print("Hello! Have a nice day.")
+
+def Bye():
+    print("Bye! See you later.")
